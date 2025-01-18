69cff76bd0dbf85a80a8a8e595f9465e225ea826
eCituk <citukcaamal@gmail.com>
2025-01-17 23:45:30 -0500

    dark mode en automatico

--- assets/js/toggler-dark-mode.js
+++ assets/js/toggler-dark-mode.js
@@ -1,21 +1,36 @@
 const toggleButton = document.getElementById('theme-toggle')
-	toggleButton.addEventListener('click', () => {
+	function toggleTheme(mode) {
 		const lightElements = document.querySelectorAll('.light-background')
 		const darkElements = document.querySelectorAll('.dark-background')
 
-		if (lightElements.length > 0) {
+		if (mode === 'dark' || (lightElements.length > 0 && mode === undefined)) {
 
 			lightElements.forEach(element => {
 				element.classList.replace('light-background', 'dark-background')
 			})
-
 			toggleButton.querySelector('i').classList.replace('bi-moon', 'bi-sun')
-		} else if (darkElements.length > 0) {
+		} else if (mode === 'light' || (darkElements.length > 0 && mode === undefined)) {
 
 			darkElements.forEach(element => {
 				element.classList.replace('dark-background', 'light-background')
 			})
-
 			toggleButton.querySelector('i').classList.replace('bi-sun', 'bi-moon')
 		}
-})
+	}
+	function detectSystemTheme() {
+		const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
+
+		if (isDarkMode) {
+			toggleTheme('dark')
+		} else {
+			toggleTheme('light')
+		}
+	}
+	window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
+		const newColorScheme = e.matches ? 'dark' : 'light'
+		toggleTheme(newColorScheme)
+	})
+	toggleButton.addEventListener('click', () => {
+		toggleTheme()
+	})
+	detectSystemTheme()
